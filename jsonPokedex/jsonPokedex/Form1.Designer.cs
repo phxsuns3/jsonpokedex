d06d25d06b86547778fe2c239bec3068f76ec8ae
Aaron Baker <acbak338@learner.dvusd.org>
2022-03-31 09:25:38 -0700

    VS

---
+++ jsonPokedex/jsonPokedex/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace jsonPokedex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.categoryLabel = new System.Windows.Forms.Label();
+            this.categoryTextBox = new System.Windows.Forms.TextBox();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.lastButton = new System.Windows.Forms.Button();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(89, 325);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(48, 328);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(35, 13);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name";
+            // 
+            // categoryLabel
+            // 
+            this.categoryLabel.AutoSize = true;
+            this.categoryLabel.Location = new System.Drawing.Point(34, 351);
+            this.categoryLabel.Name = "categoryLabel";
+            this.categoryLabel.Size = new System.Drawing.Size(49, 13);
+            this.categoryLabel.TabIndex = 2;
+            this.categoryLabel.Text = "Category";
+            // 
+            // categoryTextBox
+            // 
+            this.categoryTextBox.Location = new System.Drawing.Point(89, 348);
+            this.categoryTextBox.Name = "categoryTextBox";
+            this.categoryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.categoryTextBox.TabIndex = 3;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(8, 69);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 4;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(89, 70);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(75, 23);
+            this.readButton.TabIndex = 5;
+            this.readButton.Text = "Read";
+            this.readButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(195, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 356);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // firstButton
+            // 
+            this.firstButton.Location = new System.Drawing.Point(8, 13);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(75, 23);
+            this.firstButton.TabIndex = 7;
+            this.firstButton.Text = "First";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.firstButton_Click);
+            // 
+            // lastButton
+            // 
+            this.lastButton.Location = new System.Drawing.Point(89, 13);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new System.Drawing.Size(75, 23);
+            this.lastButton.TabIndex = 8;
+            this.lastButton.Text = "Last";
+            this.lastButton.UseVisualStyleBackColor = true;
+            this.lastButton.Click += new System.EventHandler(this.lastButton_Click);
+            // 
+            // previousButton
+            // 
+            this.previousButton.Location = new System.Drawing.Point(8, 40);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 23);
+            this.previousButton.TabIndex = 9;
+            this.previousButton.Text = "Previous";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(89, 40);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 10;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(854, 520);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.firstButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.readButton);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.categoryTextBox);
+            this.Controls.Add(this.categoryLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label categoryLabel;
+        private System.Windows.Forms.TextBox categoryTextBox;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button lastButton;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button nextButton;
+    }
+}
+
---
+++ jsonPokedex/jsonPokedex/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Collections;
+
+namespace jsonPokedex
+{
+    public partial class Form1 : Form
+    {
+        int current = 0;
+        private ArrayList pokies = new ArrayList();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Console.WriteLine(File.ReadAllText("Pokemon.json"));
+            StreamReader inFile = File.OpenText("Pokemon.json");
+            while (inFile.Peek() != -1)
+            {
+                string p = inFile.ReadLine();
+                pokemon pokie = JsonSerializer.Deserialize<pokemon>(p);
+                pokies.Add(pokie);
+                show();
+
+                //nameTextBox.Text = pokie.name;
+                //categoryTextBox.Text = pokie.category;
+            }
+                inFile.Close();
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            var p = new pokemon
+            {
+                name = nameTextBox.Text,
+                category = categoryTextBox.Text,
+                picture = pictureBox1.ImageLocation
+            };
+
+            string fileName = "Pokemon.json";
+            string jsonString = JsonSerializer.Serialize(p);
+
+            pokies.Add(p);
+
+            StreamWriter outFile = File.CreateText(fileName);
+            foreach (var item in pokies)
+            {
+                outFile.WriteLine(JsonSerializer.Serialize(item));
+            }
+
+            outFile.Close();
+
+            //File.WriteAllText(fileName, jsonString);
+            //Console.WriteLine(File.ReadAllText(fileName));
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            show();
+            //string jsonString = File.ReadAllText("Pokemon.json");
+            //pokemon pokie = JsonSerializer.Deserialize<pokemon>(jsonString);
+            //
+            //nameTextBox.Text = pokie.name;
+            //categoryTextBox.Text = pokie.category;
+        }
+        public void show()
+        {
+            pokemon p = (pokemon)pokies[current];
+            nameTextBox.Text = p.name;
+            categoryTextBox.Text = p.category;
+            if (File.Exists(p.picture)){ pictureBox1.Load(p.picture); }
+
+        }
+        private void firstButton_Click(object sender, EventArgs e)
+        {
+            current = 0;
+            show();
+        }
+
+        private void lastButton_Click(object sender, EventArgs e)
+        {
+            current = pokies.Count;
+            show();
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            if (current != pokies.Count)
+            {
+                current--;
+                show();
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (current != pokies.Count)
+            {
+                current++;
+                show();
+            }
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.Load(openFileDialog1.FileName);
+        }
+    }
+}
